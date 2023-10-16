86ca1e37c3984e614435f7c864fbd675b9e81893
Your Name <you@example.com>
2023-10-16 13:35:51 +0700

    remove package nodemailer

--- apps/sendmail/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestApplication, NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import kafkaInit from './kafka';
-async function bootstrap() {
-  const app: NestApplication = await NestFactory.create(AppModule);
-  await kafkaInit(app);
-  await app.listen(process.env.PORT);
-}
-bootstrap();
